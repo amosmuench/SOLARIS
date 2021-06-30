3d65956a5e7969203ddca9ed563a17863da1e47d
Münch <amosmuench@login.curta.zedat.fu-berlin.de>
2021-06-30 09:40:09 +0200

    scheduler

---
+++ snake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Snake
+#SBATCH --mail-user=amos.muench@fu-berlin.de  
+#SBATCH --mail-type=end
+#SBATCH --ntasks=1                           
+#SBATCH --mem-per-cpu=4096                  
+#SBATCH --time=08:00:00                         
+#SBATCH --qos=standard 
+#SBATCH --nodes=1
+
+
+
+module load Miniconda3/4.7.10
+source activate /home/amosmuench/snakemake
+source activate /home/amosmuench/snakemake/envs/snakemake_pulp/
+
+cd /home/amosmuench/Pipeline17092020/
+snakemake -j 4 --use-conda --printshellcmds --until whippet_summary_plots -k 
